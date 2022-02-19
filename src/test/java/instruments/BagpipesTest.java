37656d9e39aac78712e84c5518270ecac4a7b562
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-19 12:13:32 +0000

    Created tests for Bagpipes class

--- src/test/java/instruments/BagpipesTest.java
+++ src/test/java/instruments/BagpipesTest.java
@@ -1,4 +1,61 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BagpipesTest {
+
+    Bagpipes bagpipes;
+
+    @Before
+    public void before(){
+        bagpipes = new Bagpipes("Kilberry", "Vintage Style", InstrumentTypes.WOODWIND, "Tartan", 50000, 95000, 4);
+    }
+
+    @Test
+    public void hasManufacturer(){
+        assertEquals("Kilberry", bagpipes.getManufacturer());
+    }
+
+    @Test
+    public void hasModel(){
+        assertEquals("Vintage Style", bagpipes.getModel());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals(InstrumentTypes.WOODWIND, bagpipes.getType());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("Tartan", bagpipes.getColour());
+    }
+
+    @Test
+    public void hasBuyPrice(){
+        assertEquals(50000, bagpipes.getBuyPrice());
+    }
+
+    @Test
+    public void hasSellPrice(){
+        assertEquals(95000, bagpipes.getSellPrice());
+    }
+
+    @Test
+    public void hasReeds(){
+        assertEquals(4, bagpipes.getReeds());
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(45000, bagpipes.calculateMarkup());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("Skirl!", bagpipes.play());
+    }
 }
