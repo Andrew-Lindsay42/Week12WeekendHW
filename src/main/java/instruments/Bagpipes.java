c9704c2de1d833b92aa808976b35f82d0a7ee2e1
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-19 12:30:17 +0000

    Added Bagpipe class

--- src/main/java/instruments/Bagpipes.java
+++ src/main/java/instruments/Bagpipes.java
@@ -1,4 +1,18 @@
 package instruments;
 
-public class Bagpipes {
+public class Bagpipes extends Instrument{
+    private int reeds;
+
+    public Bagpipes(String manufacturer, String model, InstrumentTypes instrumentType, String colour, int buyPrice, int sellPrice, int reeds) {
+        super(manufacturer, model, instrumentType, colour, buyPrice, sellPrice);
+        this.reeds = reeds;
+    }
+
+    public int getReeds() {
+        return reeds;
+    }
+
+    public String play() {
+        return "Skirl!";
+    }
 }
