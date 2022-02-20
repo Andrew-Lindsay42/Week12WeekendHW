2e5e905576c75909a40180985fdff37f9e83c335
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-20 13:18:33 +0000

    Created Shop class

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,4 +1,38 @@
 package shop;
 
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name) {
+        this.name = name;
+        this.stock = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int countStock() {
+        return stock.size();
+    }
+
+    public void addItem(ISell item) {
+        stock.add(item);
+    }
+
+    public void removeItem(ISell item) {
+        stock.remove(item);
+    }
+
+    public int calculateTotalProfit() {
+        return stock.stream()
+                .map(ISell::calculateMarkup)
+                .reduce(0, Integer::sum);
+    }
 }
