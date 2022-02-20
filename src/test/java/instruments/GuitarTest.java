378fb6861ae01db66542d63dce9e9a1fc64ed721
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-20 12:19:09 +0000

    Tests for Guitar

--- src/test/java/instruments/GuitarTest.java
+++ src/test/java/instruments/GuitarTest.java
@@ -1,4 +1,61 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("Fender", "Stratocaster", InstrumentTypes.STRING, "Ultraburst", 150000, 214900, 6);
+    }
+
+    @Test
+    public void hasManufacturer(){
+        assertEquals("Fender", guitar.getManufacturer());
+    }
+
+    @Test
+    public void hasModel(){
+        assertEquals("Stratocaster", guitar.getModel());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals(InstrumentTypes.STRING, guitar.getType());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("Ultraburst", guitar.getColour());
+    }
+
+    @Test
+    public void hasBuyPrice(){
+        assertEquals(150000, guitar.getBuyPrice());
+    }
+
+    @Test
+    public void hasSellPrice(){
+        assertEquals(214900, guitar.getSellPrice());
+    }
+
+    @Test
+    public void hasStrings(){
+        assertEquals(6, guitar.getStrings());
+    }
+
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(64900, guitar.calculateMarkup());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("Bwang!", guitar.play());
+    }
 }
