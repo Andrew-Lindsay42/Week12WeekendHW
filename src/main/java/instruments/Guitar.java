4d18f12724595ada48c2eb6a18d3f94e2f1f341e
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-20 12:20:17 +0000

    Created Guitar class

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,4 +1,18 @@
 package instruments;
 
-public class Guitar {
+public class Guitar extends Instrument{
+    private int strings;
+
+    public Guitar(String manufacturer, String model, InstrumentTypes instrumentType, String colour, int buyPrice, int sellPrice, int strings) {
+        super(manufacturer, model, instrumentType, colour, buyPrice, sellPrice);
+        this.strings = strings;
+    }
+
+    public int getStrings() {
+        return strings;
+    }
+
+    public String play() {
+        return "Bwang!";
+    }
 }
