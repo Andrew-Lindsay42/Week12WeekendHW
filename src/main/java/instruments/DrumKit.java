8ba58e864a66c149e3ad4f1b22441b6148d03f8b
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-20 11:47:30 +0000

    Created DrumKit class

--- src/main/java/instruments/DrumKit.java
+++ src/main/java/instruments/DrumKit.java
@@ -1,4 +1,25 @@
 package instruments;
 
-public class DrumKit {
+public class DrumKit extends Instrument{
+
+    private int drums;
+    private int cymbals;
+
+    public DrumKit(String manufacturer, String model, InstrumentTypes instrumentType, String colour, int buyPrice, int sellPrice, int drums, int cymbals) {
+        super(manufacturer, model, instrumentType, colour, buyPrice, sellPrice);
+        this.drums = drums;
+        this.cymbals = cymbals;
+    }
+
+    public int getDrums() {
+        return drums;
+    }
+
+    public int getCymbals() {
+        return cymbals;
+    }
+
+    public String play() {
+        return "Badum tsh!";
+    }
 }
